43b6b64f631dda21e66e88b3c1597f29e4a46919
Rajendra Alluri <12444876+vamsirajendra@users.noreply.github.com>
2025-03-27 21:41:35 +0530

    added ishitham usd composer app from usd composer template

--- premake5.lua
+++ premake5.lua
@@ -15,3 +15,8 @@ repo_build.prebuild_copy {
 }
 
 -- Apps: for each app generate batch files and a project based on kit files (e.g. my_name.my_app.kit)
+
+define_app("ishitham.usd_composer.kit")
+define_app("ishitham.usd_composer_streaming.kit")
+define_app("ishitham.usd_composer_ovc.kit")
+define_app("ishitham.usd_composer_gdn_streaming.kit")
